67a47c122f0f79c733bef4c372317b6c017778b5
Peres <eueue@DESKTOP-O8GM0RL>
2025-05-18 21:31:14 +0100

    testing url

--- src/hooks/fetches/fetchChartData.js
+++ src/hooks/fetches/fetchChartData.js
@@ -1,8 +1,8 @@
-const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = import.meta.env.VITE_API_URL;
 
 export async function fetchChartData(ticker) {
     try {
-        const response = await fetch(`${API_URL}/api/stock-history/${ticker}`);
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/stock-history/${ticker}`);
         const data = await response.json();
 
         return data;
